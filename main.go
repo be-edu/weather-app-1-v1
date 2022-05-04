7361df59701497092cf14322e3ae1e1a0906f5bb
sulaga <sulaga@gmx.ch>
2022-05-04 09:36:11 +0200

    Add files

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/be-edu/weather-1/weather/v1"
+)
+
+func main() {
+	makePrediction(51.509865, -0.118092, "London")
+}
+
+func makePrediction(lat float64, long float64, locationName string) error {
+	pred, err := weather.PredictAtCoords(lat, long)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("The weather prediction for %s is: %v\n", locationName, pred.ToString())
+
+	return nil
+}
